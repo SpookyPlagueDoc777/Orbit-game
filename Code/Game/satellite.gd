d3264b009d16e7a93011a5ab8d10b7deeb30601c
McraftYT <epicstudios770@gmail.com>
2026-06-21 14:50:57 +0400

    Actual launches!!!
    satelite go brr

--- Code/Game/satellite.gd
+++ Code/Game/satellite.gd
@@ -6,32 +6,46 @@ class_name satellite
 @export var satellitename: String
 @export var health: float
 @export var orbitradius: float
-@export var baseangle: float
+var baseangle: float = PI/4
 
 @export_category("Satellite Energy")
 @export var satenergyprod: int
 @export var satenergylaunch: int
 @export var satspinprod: float
+var orbitspeed: float
+var launchanimation: bool = true
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	var tween: Tween = create_tween()
+	tween.set_ease(Tween.EASE_OUT)
+	tween.set_trans(Tween.TRANS_SINE)
+	$".".position = Vector2(50, -260)
+	print(position)
+	orbitspeed = sqrt(Global.G * Global.planetmass / orbitradius)
+	print(Vector2(orbitradius/cos(baseangle), -orbitradius/sin(baseangle)))
+	tween.tween_property($".","position", Vector2(orbitradius * cos(baseangle), -orbitradius * sin(baseangle)), sqrt(orbitradius)/10)
+	tween.emit_signal("finished")
+	tween.connect("finished", _on_animation_finished)
+	print(baseangle)
+	
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	orbit_planet()
+	if launchanimation == false:
+		orbit_planet()
 
 func orbit_planet() -> void:
-	var orbitspeed: float = sqrt(Global.G * Global.planetmass / orbitradius)
 	baseangle = fmod(baseangle + orbitspeed * get_process_delta_time() / orbitradius, (2 * PI))
-	var x_pos: float = cos(baseangle)
-	var y_pos: float = sin(baseangle)
 	
-	position.x = orbitradius * x_pos
-	position.y = orbitradius * y_pos
+	position.x = orbitradius * cos(baseangle - PI/2)
+	position.y = orbitradius * sin(baseangle - PI/2)
 
 
 func _on_area_2d_area_entered(area: Area2D) -> void:
 	health -= 1
 	if health <= 0:
 		get_tree().queue_delete($".")
+
+func _on_animation_finished() -> void:
+	launchanimation = false
--- Code/Ui/pause_menu.gd
+++ Code/Ui/pause_menu.gd
@@ -1,22 +1,22 @@
 extends Control
-
-@onready var resume: Button = $Pause_menu/Panel/VBoxContainer/resume
-@onready var settings: Button = $Pause_menu/Panel/VBoxContainer/settings
-@onready var quit: Button = $Pause_menu/Panel/VBoxContainer/quit
-@onready var clicksound: AudioStreamPlayer = $"../Audio/Clicksound"
-
-func _ready() -> void:
-	pass
-
-func _on_resume_pressed() -> void:
-	get_tree().paused = false
-	visible = false
-	clicksound.play()
-
-func _on_settings_pressed() -> void:
-	Global.updatemenu.emit(Global.menus.SETTINGS)
-	clicksound.play()
-	
-func _on_quit_pressed() -> void:
-	clicksound.play()
-	get_tree().quit()
+#
+#@onready var resume: Button = $Pause_menu/Panel/VBoxContainer/resume
+#@onready var settings: Button = $Pause_menu/Panel/VBoxContainer/settings
+#@onready var quit: Button = $Pause_menu/Panel/VBoxContainer/quit
+#@onready var clicksound: AudioStreamPlayer = $"../Audio/Clicksound"
+#
+#func _ready() -> void:
+	#pass
+#
+#func _on_resume_pressed() -> void:
+	#get_tree().paused = false
+	#visible = false
+	#clicksound.play()
+#
+#func _on_settings_pressed() -> void:
+	#Global.updatemenu.emit(Global.menus.SETTINGS)
+	#clicksound.play()
+	#
+#func _on_quit_pressed() -> void:
+	#clicksound.play()
+	#get_tree().quit()
