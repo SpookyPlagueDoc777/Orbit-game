e71500c995e1f54be760b1beebe261919db84af2
SpookyPlagueDoc777 <mrc.versari@gmail.com>
2026-06-25 23:13:53 +0200

    view satellites in shop

--- Code/Ui/game_ui.gd
+++ Code/Ui/game_ui.gd
@@ -6,6 +6,7 @@ var last_state: bool
 @onready var shop_menu: Control = $Shop_ui/ShopMenu
 @onready var pause_button: Button = $PauseButton/PauseButton
 @onready var satellite_container: VBoxContainer = $SatelliteList/VBoxContainer/SatelliteScroll/SatelliteContainer
+@onready var satellite_container2: VBoxContainer = $Shop_ui/ShopMenu/ActualButtons/SatellitesList/VBoxContainer/SatelliteScroll/SatelliteContainer
 
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("ui_cancel") and visible == false:
@@ -39,3 +40,16 @@ func update_satellite_list() -> void:
 		var satelitus = SATELLITE_INFO.instantiate()
 		satellite_container.add_child(satelitus)
 		satelitus.update_info(member)
+<<<<<<< Updated upstream
+=======
+		
+	
+	for member in satellite_container2.get_children():
+		member.queue_free()
+	for member in satellites:
+		var satelitus2 = SATELLITE_INFO.instantiate()
+		satellite_container2.add_child(satelitus2)
+		satelitus2.update_info(member)
+		
+	print(satellites)
+>>>>>>> Stashed changes
