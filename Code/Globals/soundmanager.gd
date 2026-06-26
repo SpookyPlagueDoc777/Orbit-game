d64cbd28b6e11d3cbf29fb7087ea224de79a7dde
McraftYT <epicstudios770@gmail.com>
2026-06-27 00:24:41 +0400

    added music
    -it makes it better ig

--- Code/Globals/soundmanager.gd
+++ Code/Globals/soundmanager.gd
@@ -1,16 +1,18 @@
 extends Node
 
 @onready var sfx_player: AudioStreamPlayer2D = $"../MainNode/CanvasLayer/SFXPlayer"
+@onready var music_player: AudioStreamPlayer2D = $"../MainNode/CanvasLayer/MusicPlayer"
 
 const CLICK = preload("uid://nkcnhb6kawop")
 const DECLICK = preload("uid://bj7fvc1pjyr66")
+const MUSIC1 = preload("uid://bdv8nduhuyp63")
 
 var clicksound: = CLICK.instantiate_playback()
 var declicksound: = DECLICK.instantiate_playback()
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	play_music(MUSIC1)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -18,11 +20,15 @@ func _process(delta: float) -> void:
 	pass
 
 func successful_push() -> void:
-	play_sound(CLICK)
+	play_sfx(CLICK)
 
 func unsuccessful_push() -> void:
-	play_sound(DECLICK)
+	play_sfx(DECLICK)
 
-func play_sound(sound): 
+func play_sfx(sound): 
 	sfx_player.stream = sound
 	sfx_player.play()
+
+func play_music(sound): 
+	music_player.stream = sound
+	music_player.play()
